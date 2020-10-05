0e2134f26faffa1fecb5120b0e2b6fef7a9c2b41
Sean Guy <sean.guy@outlook.com>
2020-10-05 11:04:00 -0400

    Corrected 'image name' to 'image_name'

--- run.py
+++ run.py
@@ -7,10 +7,10 @@ import requests
 
 #feedback =
 url = "http://34.121.212.0/fruits/"
-files = os.listdir("/supplier-data/descriptions/")
+files = os.listdir("./supplier-data/descriptions/")
 for file in files:
         entry = collections.OrderedDict()
-        path = os.path.join("/supplier-data/descriptions/", file)
+        path = os.path.join("./supplier-data/descriptions/", file)
         print(path)
         name = file.split('.')
         imagename = "{}.jpeg".format(name[0])
@@ -19,7 +19,7 @@ for file in files:
                 entry['name'] = lines[0].strip("[\n]")
                 entry['weight'] = int(lines[1].strip("[ lbs][\n]"))
                 entry['description'] = lines[2].strip("[\n]")
-                entry['image name'] = imagename
+                entry['image_name'] = imagename
                 print(entry)
                 feedback_json = json.dumps(entry)
                 print(feedback_json)
