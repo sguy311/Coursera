424b37e12e66c6f327ef4f388ab6546fb8a46788
Sean Guy <sean.guy@outlook.com>
2020-10-07 12:06:12 -0400

    updated final pdf scripts

--- report_email.py
+++ report_email.py
@@ -1,27 +1,65 @@
 #!/usr/bin/env python3
-
+import reports
 import datetime
 import os
 import sys
 import json
+import collections
+import noemail
 
-#path = "./supplier-data/descriptions/"
-path = 'C:\\Users\\seang\\Documents\\007.txt'
+path = "/home/godsack/supplier-data/descriptions/"
+files = os.listdir(path)
+#print(files)
 data = {}
+date = datetime.datetime.now()
+title = "Processed Update on {}".format(date.strftime("%B %d %Y"))
+print(title)
+sender = "automation@example.com"
+recipient = "{}@example.com".format(os.environ.get('USER'))
+subject = "Upload Completed - Online Fruit Store"
+body = "All fruits are uploaded to our website successfully. A detailed list is attached to this email."
 def load_data(filename):
     """Loads the contents of filename as a JSON file."""
-    with open(filename) as file:
-        lines = file.readlines()
-        data['name'] = lines[0].strip("[\n]")
-        data['weight'] = lines[1].strip("[\n]")
+    #entry = {}
+    for object in filename:
+        #print(object)
+        id = object.split('.')
+        txt = os.path.join(path,object)
+        #print(txt)
+        with open(txt, 'r') as item:
+            lines = item.readlines()
+            #print(lines)
+            data[id[0]] = {}
+            data[id[0]]['name'] = lines[0].strip("[\n]")
+            data[id[0]]['weight'] = lines[1].strip("[\n]")
     return data
+
+def fruit_dict_to_table(car_data):
+    print(car_data)
+    """Turns the data in car_data into a list of lists."""
+    table_data = []
+    for item in car_data:
+        #print(item)
+        tname = "Name: {}".format(car_data[item]['name'])
+        tweight = "Weight: {}".format(car_data[item]['weight'])
+        table_data.append([tname])
+        table_data.append([tweight])
+        table_data.append("\n")
+        #table_data.append([car_data[item]["name"], car_data[item]["weight"]])
+    return table_data 
 
 def process_data():
     pass
 
 def main():
-    pdfdata = load_data(path)
-    print(pdfdata)
+    #for file in files:
+    pdfdata = load_data(files)
+    #print(pdfdata)
+    table = fruit_dict_to_table(pdfdata)
+    print(table)
+    reports.generate_report("/tmp/processed.pdf",title,table)
+    email = noemail.generate_email(sender,recipient,subject,body,"/tmp/processed.pdf")
+
 
 if __name__ == "__main__":
     main()
